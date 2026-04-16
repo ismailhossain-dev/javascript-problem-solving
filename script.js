383864472bfa66840ff5f7391e513a83201b91ba
sabbir <programmarsabbir@gmail.com>
2026-04-17 01:17:16 +0800

    complete-problem-1

---
+++ script.js
@@ -0,0 +1,7 @@
+//problem-1 => object convert to string
+const obj = { a: 1, b: 2 };
+//console.log(typeof obj); //object
+const convertString = JSON.stringify(obj); //convert string
+//console.log(typeof convertString); //out put string
+
+//problem-2 array convert string
